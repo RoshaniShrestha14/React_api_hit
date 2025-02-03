89ab687f0e065af01e3878d77071f736b4432638
Roshani2211205 <roshani2211205@iimscollege.edu.np>
2025-02-03 10:13:30 +0545

    Initial Commit

---
+++ api-hit/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginForm from './components/loginform'
+
+const App = () => {
+  return (
+    <div>
+     <LoginForm></LoginForm>
+    </div>
+  )
+}
+
+export default App
